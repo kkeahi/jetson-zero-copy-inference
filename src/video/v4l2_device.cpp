3ea0b83d80135992646f2287503c3bc3826d05e8
keahi <trenstonricks@gmail.com>
2025-12-28 20:14:58 -1000

    Made constructor way better

--- src/video/v4l2_device.cpp
+++ src/video/v4l2_device.cpp
@@ -14,26 +14,50 @@
 
 namespace jetson_middleware { 
 
-V4L2Device::V4L2Device(const char* path, uint32_t width, uint32_t height) {
-	_set_params(path, width, height);
+V4L2Device::V4L2Device(const char* path, uint32_t width, uint32_t height)
+	: _path(path), _width(width), _height(height) 
+{
+	_fd = open(path, 0);
+	if (_fd < 0) {} // err
+	
 
-	_validate_capabilities(_path);
+	// query capabilities //
+	v4l2_capability cap {};
+	if (xioctl(_fd, VIDIOC_QUERYCAP, &cap) < 0) {} // err
+	
+	const bool is_capture = 
+		(cap.capabilities & V4L2_CAP_VIDEO_CAPTURE) || 	// make sure device supports video capture
+		(cap.device_caps & V4L2_CAP_VIDEO_CAPTURE); 	// check physical device & specific opened fd
+	
+	const bool is_streaming = 
+		(cap.capabilities & V4L2_CAP_STREAMING) || 	// make sure device supports streaming I/O via user pointers
+		(cap.device_caps & V4L2_CAP_STREAMING);
 
-	// figure out which V4L2_MEMORY_x ioctl to use
-	// validate that ioctl against the device
-
-	_set_format();
+	if (!is_capture) {} 	// err
+	if (!is_streaming) {} 	// err
+	
+	// set device format //
+	v4l2_format fmt {};
+	fmt.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+	fmt.fmt.pix.width = _width;
+	fmt.fmt.pix.height = _height;
+	fmt.fmt.pix.pixelformat = V4L2_PIX_FMT_YUYV;	// may need customization
+	fmt.fmt.pix.field = V4L2_FIELD_NONE;		// may need customization
+	
+	if (xioctl(_fd, VIDEOC_S_FMT, &fmt) < 0) {} // err
+	
+	// VIDIO_C_FMT may change fmt
+	_width = fmt.fmt.pix.width;
+	_height = fmt.fmt.pix.height;
+	// may need one for pixelformat
 }
 
 V4L2Device::~V4L2Device() {
-	int res = close(_fd);
-	if (res == -1) {
-		int err = errno;
-		fprintf(stderr, "ERROR: failed to close fd %d: %s\n", _fd, strerror(err));
-	}
 }
 
-void V4L2Device::request_buffers(uint32_t count) {}
+void V4L2Device::request_buffers(uint32_t count) {
+	
+}
 
 void V4L2Device::enqueue_frame() {}
 
